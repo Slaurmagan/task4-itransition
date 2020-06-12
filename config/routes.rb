11b71583541f9e9252e96b26333ddcf5fed7b205
Slaurmagan <s.hrakovich33@mail.ru>
2020-06-13 01:03:32 +0300

    Fixed bug with requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root to: 'public#index'
-  get '/block' => "public#block"
-  get '/delete_users' => 'public#delete'
+  post '/block' => "public#block"
+  post '/delete_users' => 'public#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 	
