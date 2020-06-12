afae8ad4a936cc16021f110fb29946f561359e70
Slaurmagan <s.hrakovich33@mail.ru>
2020-06-12 19:44:16 +0300

    Added stimulusjs, added checkbox functional

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,27 @@
+// Visit The Stimulus Handbook for more details 
+// https://stimulusjs.org/handbook/introduction
+// 
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "checkbox","user_id" ]
+
+  connect() {
+    
+  }
+  check_all() {
+  	this.checkboxTargets.forEach(target => target.checked = !target.checked)
+  }
+  block(e) {
+  	e.preventDefault()
+  }
+  delete(e){
+  	e.preventDefault()
+  }
+}
